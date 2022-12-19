d4b9f482c48cf4ec54859b767aff6c7190ae84cc
git/rayhanrobbani <rayhan@lskit.com>
2022-12-19 16:24:57 +0600

    search-content-refactored

--- resources/views/frontend/src/pages/search/index.blade.php
+++ resources/views/frontend/src/pages/search/index.blade.php
@@ -17,19 +17,31 @@
                     <div>
                         <h6>{{ $post->title['en'] }}</h6>
                         <img src="{{ asset("uploads/post/$post->image") }}" alt="{{ $post->title['en'] }}" width="200px">
-                        <a href="/collection_details/{{$post->id}}">See more</a>
+                        <a href="/collection_details/{{ $post->id }}">See more</a>
                     </div>
                 @endforeach
             </div>
         @endif
 
-        @if (isset($posts))
+        @if (isset($collections))
             <div>
                 <h3>Collection</h3>
                 @foreach ($collections as $collection)
                     <div>
-                        <h6>{{ $collection->meta_data['description'] }}</h6>
-                        <img src="{{ asset("uploads/post/$collection->post_id/$collection->image") }}" alt="{{ $collection->meta_data['description'] }}" width="200px">
+                        <h6>
+                            @if (isset($collection->meta_data['description']['al']))
+                                {!! $collection->meta_data['description']['al'] !!},
+                            @elseif(!is_array($collection->meta_data['description']))
+                                {!! $collection->meta_data['description'] !!},
+                            @endif
+                        </h6>
+                        <img src="{{ asset("uploads/post/$collection->post_id/$collection->image") }}"
+                            alt="@if (isset($collection->meta_data['description']['al']))
+                                    {!! $collection->meta_data['description']['al'] !!},
+                                @elseif(!is_array($collection->meta_data['description']))
+                                    {!! $collection->meta_data['description'] !!},
+                                @endif"
+                            width="200px">
                         <a href="/collection/{{$collection->post_id }}/gallery/{{ $collection->id }}">See more</a>
                     </div>
                 @endforeach
@@ -43,7 +55,7 @@
                     <div>
                         <h6>{{ $author->name }}</h6>
                         <img src="{{ asset("uploads/author/$author->image") }}" alt="{{ $author->name }}" width="200px">
-                        <a href="/author/{{$author->id}}">See more</a>
+                        <a href="/author/{{ $author->id }}">See more</a>
                     </div>
                 @endforeach
             </div>
@@ -55,7 +67,8 @@
                 @foreach ($reportages as $reportage)
                     <div>
                         <h6>{{ $reportage->name['en'] }}</h6>
-                        <img src="{{ asset("uploads/reportage/$reportage->image") }}" alt="{{ $reportage->name['en'] }}" width="200px">
+                        <img src="{{ asset("uploads/reportage/$reportage->image") }}" alt="{{ $reportage->name['en'] }}"
+                            width="200px">
                         <a href="/reportage/{{ $reportage->id }}">See more</a>
                     </div>
                 @endforeach
@@ -68,7 +81,8 @@
                 @foreach ($categories as $category)
                     <div>
                         <h6>{{ $category->name['en'] }}</h6>
-                        <img src="{{ asset("uploads/category/$category->image") }}" alt="{{ $category->name['en'] }}" width="200px">
+                        <img src="{{ asset("uploads/category/$category->image") }}" alt="{{ $category->name['en'] }}"
+                            width="200px">
                         <a href="/singleCategory/{{ $category->id }}">See more</a>
                     </div>
                 @endforeach
